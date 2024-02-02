6ee215a8480f0196c957f4e89993799898dba6da
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-02 19:20:36 +0530

    Variables in Python

---
+++ Basics/Variables/p2.py
@@ -0,0 +1,12 @@
+# Python allows you to assign values to multiple variables in one line
+x, y, z = "Red", "Blue", "Green"
+print(x)
+print(y)
+print(z)
+
+# You can assign the Same value to multiple variables in one line
+x = y = z = "Python Class"
+print(x, y, z)
+print(x + y + z)
+
+
