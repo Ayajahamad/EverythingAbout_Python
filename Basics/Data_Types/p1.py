87f0398623807ed2be88c24afc28c6339c64fe46
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-04 22:31:41 +0530

    Example for datatypes

---
+++ Basics/Data_Types/p1.py
@@ -0,0 +1,31 @@
+a = 2
+## Find the type 
+a1 = type(a)
+print(a1)
+
+
+# Numeric Types
+num_int = 5
+num_float = 3.14
+num_complex = 3 + 2j
+
+print(type(num_complex))
+print(num_complex)
+
+# Sequence Types
+str_data = "Hello, Python!"
+list_data = [1, 'two', 3.0]
+tuple_data = (1, 'two', 3.0)
+
+# Mapping Type
+dict_data = {'name': 'John', 'age': 30}
+
+# Set Types
+set_data = {1, 2, 3, 3}
+frozen_set_data = frozenset({1, 2, 3})
+
+# Boolean Type
+bool_data = True
+
+# None Type
+none_data = None
