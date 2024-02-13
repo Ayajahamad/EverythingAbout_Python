6b0124fe97754ba0954024285f86b663ebd41ce5
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-13 13:06:16 +0530

    Simple Examples about Operators

---
+++ Basics/Operators/p1.py
@@ -0,0 +1,11 @@
+# Arithmetic Operators
+x = 10
+y = 5
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x % y)  # Modulus
+print(x ** y) # Exponentiation
+print(x // y) # Floor Division
+
