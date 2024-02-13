b153205ee4bd1a0fb2d8a0dd8d224d690c62bc53
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-13 13:06:32 +0530

    Simple Examples about Operators

---
+++ Basics/Operators/p4.py
@@ -0,0 +1,12 @@
+# Assignment Operators
+
+x = 10
+y = 5
+
+x += 1  # Add and assign
+x -= 1  # Subtract and assign
+x *= 2  # Multiply and assign
+x /= 2  # Divide and assign
+x %= 3  # Modulus and assign
+x **= 2 # Exponentiate and assign
+x //= 3 # Floor divide and assign
