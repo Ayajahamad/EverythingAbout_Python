32d5c17ff8f5fffc9e1fad981a8672ac9659884a
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-15 22:24:39 +0530

    Accept an Integer Check Whether Even or Odd Number ?

---
+++ Basics/Question-Solving/p3.py
@@ -0,0 +1,8 @@
+# 3. Accept an Integer Check Whether Even or Odd Number ?
+
+num = int(input("Enter a Number :: "))
+
+if num%2 == 0:
+    print(f"The number {num} is an Even Number")
+else:
+    print(f"The number {num} is not an Even Number")
