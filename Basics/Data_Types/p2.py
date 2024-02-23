5feb63e52433eb5c5284e5aa37ef5c41749a54f8
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-23 22:51:57 +0530

    DataType Example is added

---
+++ Basics/Data_Types/p2.py
@@ -0,0 +1,24 @@
+## You can assign a multiline string to a variable by using three quotes
+
+a = """IM learning,
+python to develop,
+an application"""
+
+print(a)
+
+## When you compare two values, the expression is evaluated returns the Boolean answer “true” or “false”.
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+a=6
+c = a>5
+print(c)
+
+##Getting the type of a value
+
+type(100)
+type(2.0)
+type('Hello')
+type(True)
+
+
