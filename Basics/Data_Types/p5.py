1c2e73be275117262ded12b4828eecf7e93649b0
Ayajahamad <ayazahmad2362001@gmail.com>
2024-03-16 09:53:36 +0530

    Python DataTyypes are added

---
+++ Basics/Data_Types/p5.py
@@ -0,0 +1,11 @@
+## ord() - function converted to unicode formate
+
+print(ord("a"))
+
+print("The ASCII value of 9 is",ord('9')) 
+
+# unicode code point of alphabet 
+print("The ASCII value of B is",ord('B'))  
+
+# unicode code point of special character 
+print("The ASCII value of ! is",ord('!'))
