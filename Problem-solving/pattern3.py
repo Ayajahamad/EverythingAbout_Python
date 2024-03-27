892889f03fbbce7b6ec83c25e5e49dff49140980
Ayajahamad <ayazahmad2362001@gmail.com>
2024-03-27 19:25:04 +0530

    Python Pattern is added

---
+++ Problem-solving/pattern3.py
@@ -0,0 +1,18 @@
+# Equilateral Triangle Pattern
+
+rows = 6
+for i in range(rows):
+    for j in range(rows-i-1):
+        print(" ",end='')
+    for k in range(i+1):
+        print('*',end=' ')
+    print()
+
+print()
+
+for i in range(rows):
+    for j in range(i):
+        print(' ', end="")
+    for k in range(rows-i):
+        print('*',end=' ')
+    print()
