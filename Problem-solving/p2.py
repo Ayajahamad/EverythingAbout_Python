2151e6d50c13d0b3c758b08d47d6a1e09eff95ed
Ayajahamad <ayazahmad2362001@gmail.com>
2024-03-29 14:12:30 +0530

    Sum of 2 space Separated numbers

---
+++ Problem-solving/p2.py
@@ -0,0 +1,19 @@
+class CodeTikki:
+    def solution(self):
+        # Write your solution here
+        inputs = input().split()
+        print(inputs)
+        A = int(inputs[0])
+        B = int(inputs[1])
+
+        print(inputs[0])
+        print(inputs[1])
+
+        
+        # Add numbers
+        result = A + B
+        
+        # Print result
+        print(result)
+# Runner
+CodeTikki().solution()
